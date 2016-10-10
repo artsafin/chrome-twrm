dde72b958ab69be1a73f38824a8750128e345046
Artur Safin <artur.safin@forextime.com>
2016-10-10 18:34:15 +0300

    Add field summary indicator

--- src/js/rm_inject.js
+++ src/js/rm_inject.js
@@ -37,5 +37,47 @@ configure(function(config){
             $('#content').find('.contextual:first').prepend(addTwReferenceLink);
         }
     }
+
+    $(function(){
+        new FieldSummaryIndicator()
+    });
 });
 
+var FieldSummaryIndicator = (function(){
+    function getCellsSum(tdSelector) {
+        return $('.context-menu-selection td.' + tdSelector).map(function () {
+            try {
+                return parseFloat(this.innerText);
+            }
+            catch (e) {
+                return 0;
+            }
+        }).get().reduce(function (acc, it) {
+            return acc + (isNaN(it) ? 0 : it);
+        }, 0);
+    }
+
+    function getHtml() {
+        var html = '<div id="fieldIndicator" style="width: 200px; height: 40px; position: fixed; background: white; border: 1px #ccc solid; bottom: 0; right: 0; box-shadow: -10px -7px 47px -15px rgba(120, 120, 120, 1);">'
+                   + '<b>Est:</b>&nbsp;<span id="est">0</span><br><b>Spent:</b>&nbsp;<span id="spent">0</span>'
+                   + '</div>';
+
+        return html;
+    }
+
+    var cls = function(){
+        var el = $(getHtml());
+
+        $(document.body).append(el);
+
+        $(document).on('click', 'tr', function () {
+            var est = getCellsSum('estimated_hours'),
+                spent = getCellsSum('spent_hours');
+
+            el.find('#est').text(est);
+            el.find('#spent').text(spent);
+        });
+    };
+
+    return cls;
+})();
